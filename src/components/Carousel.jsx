c149ea7deb1a17caf62e6735fea2562631da2e20
KhurramSagoo <uaf.khurram@gmail.com>
2023-12-03 18:21:01 +0500

    axios added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css'
-import photo1 from '../assets/photo1.jpg'
-import photo2 from '../assets/photo2.jpg'
-import photo3 from '../assets/photo3.jpg'
-import photo4 from '../assets/photo4.jpg'
-import photo5 from '../assets/photo5.jpg'
-import photo6 from '../assets/photo6.jpg'
-import photo7 from '../assets/photo7.jpg'
+import { useState } from 'react';
 import './carousel.css'
 
 const Carousel = () => {
+  const [api, setApi] = useState([])
+
+  const getPicsum=async ()=>{
+  try {
+    const res=await axios.get("https://picsum.photos/v2/list")
+    const data= res.data
+    setApi(data)
+    // console.log(data)
+  } catch (error) {
+    console.log(error)
+    
+  }
+  }
+
   const settings = {
     items: 1,
     loop: true,
@@ -25,35 +34,19 @@ const Carousel = () => {
     dotContainerClass: "owl-dot-container",
 
   };
-  const data = [
 
-    {
-      id: 1,
-      text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cupiditate, necessitatibus ",
-      path: photo4,
-    },
-    {
-      id: 2,
-      text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cupiditate, necessitatibus  ",
-      path: photo5,
-    },
-    {
-      id: 3,
-      text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cupiditate, necessitatibus  ",
-      path: photo6,
-    },
-
-
-  ];
-
+useEffect(()=>{
+  getPicsum();
+}
+,[])
   return (
 
 
     <OwlCarousel className="owl-theme" {...settings}>
-      {data.map((item, id) => (
-        <div key={id}>
-          <p className='txt'>{item.text} </p>
-          <img className='img' src={item.path} alt={item.id} />
+      {api.map((item, id) => (
+        <div key={id} className='main-div'>
+          <p className='txt'>{item.author} </p>
+          <img className='img' src={item.download_url} alt={item.url} />
         </div>
 
       ))}
